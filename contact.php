ceeda9a3013145a2c4633de1d0817b6b2aa9a18d
Jonathon Park <jonathonpark91@gmail.com>
2014-09-25 02:54:18 -0700

    added all projects and also linkedin and git

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php 
-$emailTo = 'yourmail@example.com';
-$siteTitle = 'YourSiteTitle';
+$emailTo = 'jonathonpark91@gmail.com';
+$siteTitle = 'JonathonPark';
 
 error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
 
